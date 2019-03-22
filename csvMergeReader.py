2edf04ede00962848039ac83be2d1b9aea3cb59d
chandhya <chandhya.ts@gmail.com>
2019-03-22 17:28:46 -0500

    Updated to read 100,000 samples from each file

--- csvMergeReader.py
+++ csvMergeReader.py
@@ -11,41 +11,36 @@ import glob
 entries = []
 bwstart = 0
 bwend = 500
-
 files = os.listdir("./csvFiles/")
 for file in files:
 	print file
-	for x in range(200):
+
+	for x in range(100):
 		if file.startswith("baseline_1"):
 			print file
 			with open('./csvFiles/baseline_1.mat.csv', 'r') as f:
 			    mycsv = csv.reader(f)
 			    for row in itertools.islice(mycsv, bwstart,bwend):
 			        entries.append(row)
-
 		if file.startswith('InnerRaceFault_vload_1'):
 			print file
 			with open('./csvFiles/InnerRaceFault_vload_1.mat.csv', 'r') as f:
 			    mycsv = csv.reader(f)
 			    for row in itertools.islice(mycsv, bwstart,bwend):
 			        entries.append(row)
-
 		if file.startswith('OuterRaceFault_1'):
 			print file
 			with open('./csvFiles/OuterRaceFault_1.mat.csv', 'r') as f:
 			    mycsv = csv.reader(f)
 			    for row in itertools.islice(mycsv, bwstart,bwend):
 			        entries.append(row)
-
 		if file.startswith('OuterRaceFault_vload_1'):
 			print file
 			with open('./csvFiles/OuterRaceFault_vload_1.mat.csv', 'r') as f:
 			    mycsv = csv.reader(f)
 			    for row in itertools.islice(mycsv, bwstart,bwend):
 			        entries.append(row)
-		bwstart = bwend + 1
-	    	bwend = bwstart + 500
-		print x
-with open('Baseline_faulty_1_merged.csv', 'wb') as fileWriter:
-	writer = csv.writer(fileWriter)
+
+with open('BaseLine-Faulty-merged.csv', 'wb') as f:
+	writer = csv.writer(f)
 	writer.writerows(entries)
