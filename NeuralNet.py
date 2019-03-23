48766e1dbd5e940a7e9d3a5dc1f5c8cf3755978e
MaxChillin <jeremy.pierce0011@gmail.com>
2019-03-22 21:45:27 -0500

    Update NeuralNet.py

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,5 +1,8 @@
+from os.path import dirname
+from os import listdir
 import tensorflow as tf
 import numpy as np
+from random import randint
 import keras
 from keras import backend as K
 from keras.models import Sequential
@@ -8,21 +11,129 @@ from keras.layers.core import Dense
 from keras.optimizers import Adam
 from keras.metrics import categorical_crossentropy
 from sklearn.preprocessing import MinMaxScaler
+import csv
+import itertools
+import pandas as pd
+
+train_labels = []
+train_samples = []
+train_samples_row_1 = []
+train_samples_row_2 = []
+train_samples_row_3 = []
+test_samples = []
+test_samples_row_1 = []
+test_samples_row_2 = []
+test_samples_row_3 = []
+test_labels = []
+
+counter = 0
+
+with open('BaseLine-Faulty-merged.csv', 'r') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        counter = counter + 1
+        if(counter < 40000):
+            train_samples_row_1.append(row[0])
+            train_samples_row_2.append(row[1])
+            train_samples_row_3.append(row[2])
+            train_labels.append(row[3])
+        else:
+            test_samples_row_1.append(row[0])
+            test_samples_row_2.append(row[1])
+            test_samples_row_3.append(row[2])
+            test_labels.append(row[3])
+
+#for i in range(5000):
+#    random_younger = randint(13,64)
+#    train_samples.append(random_younger)
+#    train_labels.append(0)
+
+#    random_older = randint(65,100)
+#    train_samples.append(random_older)
+#    train_labels.append(1)
+
+#for i in range(1000):
+#    random_younger = randint(13,64)
+#    train_samples.append(random_younger)
+#    train_labels.append(1)
+
+#    random_older = randint(65,100)
+#    train_samples.append(random_older)
+#    train_labels.append(0)
+
+train_samples_row_1 = np.array(train_samples_row_1)
+train_samples_row_2 = np.array(train_samples_row_2)
+train_samples_row_3 = np.array(train_samples_row_3)
+train_labels = np.array(train_labels)
+
+#for i in range(10):
+#    random_younger = randint(13,64)
+#    test_samples.append(random_younger)
+#    test_labels.append(1)
+
+#    random_older = randint(65,100)
+#    test_samples.append(random_older)
+#    test_labels.append(0)
+
+#for i in range(200):
+#    random_younger = randint(13,64)
+#    test_samples.append(random_younger)
+#    test_labels.append(0)
+
+#    random_older = randint(65,100)
+#    test_samples.append(random_older)
+#    test_labels.append(1)
+
+test_samples_row_1 = np.array(test_samples_row_1)
+test_samples_row_2 = np.array(test_samples_row_2)
+test_samples_row_3 = np.array(test_samples_row_3)
+test_labels = np.array(test_labels)
+
+
+
+BATCH_SIZE = 10
+EPOCHS = 550
 
 neuralNet = Sequential([
-    Dense(16, input_shape=(1,), activation='relu'), # This is the input layer
+    Dense(16, input_shape=(3,), activation='relu'), # This is the input layer
     Dense(32, activation='relu'),                   # This is the first hidden layer
-    Dense(2, activation='softmax'),                    # This is the output layer
+    Dense(3, activation='softmax'),                 # This is the output layer
     ])
 
-train_data = []
-train_labels = []
-test_data = []
 
 scaler = MinMaxScaler(feature_range=(0,1))
-scaled_train_data = scaler.fit_transform((train_data).reshape(-1,1))
-
+scaled_train_sample_row_1 = scaler.fit_transform((train_samples_row_1).reshape(-1,1))
+scaled_train_sample_row_2 = scaler.fit_transform((train_samples_row_2).reshape(-1,1))
+scaled_train_sample_row_3 = scaler.fit_transform((train_samples_row_3).reshape(-1,1))
+scaled_test_sample_row_1 = scaler.fit_transform((test_samples_row_1).reshape(-1,1))
+scaled_test_sample_row_2 = scaler.fit_transform((test_samples_row_2).reshape(-1,1))
+scaled_test_sample_row_3 = scaler.fit_transform((test_samples_row_3).reshape(-1,1))
 
 neuralNet.compile(Adam(lr=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-neuralNet.fit(scaled_train_data, train_labels, batch_size=10, epochs=20, shuffle=True, verbose=2)
+for i in scaled_train_sample_row_1:
+    train_samples = str(scaled_train_sample_row_1) + str(scaled_train_sample_row_2) + str(scaled_train_sample_row_3)
+
+for i in scaled_test_sample_row_1:
+    test_samples = str(scaled_test_sample_row_1) + str(scaled_test_sample_row_2) + str(scaled_test_sample_row_3)
+
+
+#valid_set = [(sample, value),(sample, value),(sample, value),(sample, value)]
+#validation_split = 0.1
+neuralNet.fit(train_samples, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS, shuffle=True, verbose=2)
+
+predictions = neuralNet.predict(test_samples, batch_size=10, verbose=0)
+
+rounded_prediction = neuralNet.predict_classes(test_samples, batch_size=10, verbose=0)
+
+correct = 0
+incorrect = 0
+
+for i in range(210):
+    print("Prediction = " + str(rounded_prediction[i]) + " True Value = " + str(test_labels[i]))
+    if(rounded_prediction[i] == test_labels[i]):
+        correct = correct + 1
+    else:
+        incorrect = incorrect + 1
+
+print("Correct", correct, "Incorrect", incorrect)
