761fa6d37186bf88ddf0e69a6c64e98654ce75de
Ujjwal <ujjwalmishra7@gmail.com>
2019-03-22 20:17:07 -0500

    csvmergerreader wip

--- csvMergeReader.py
+++ csvMergeReader.py
@@ -7,39 +7,46 @@ import os
 import csv
 import itertools
 import glob
+import random
 
 entries = []
 bwstart = 0
 bwend = 500
 files = os.listdir("./csvFiles/")
-for file in files:
-	print file
 
-	for x in range(100):
-		if file.startswith("baseline_1"):
-			print file
-			with open('./csvFiles/baseline_1.mat.csv', 'r') as f:
-			    mycsv = csv.reader(f)
-			    for row in itertools.islice(mycsv, bwstart,bwend):
-			        entries.append(row)
-		if file.startswith('InnerRaceFault_vload_1'):
-			print file
-			with open('./csvFiles/InnerRaceFault_vload_1.mat.csv', 'r') as f:
-			    mycsv = csv.reader(f)
-			    for row in itertools.islice(mycsv, bwstart,bwend):
-			        entries.append(row)
-		if file.startswith('OuterRaceFault_1'):
-			print file
-			with open('./csvFiles/OuterRaceFault_1.mat.csv', 'r') as f:
-			    mycsv = csv.reader(f)
-			    for row in itertools.islice(mycsv, bwstart,bwend):
-			        entries.append(row)
-		if file.startswith('OuterRaceFault_vload_1'):
-			print file
-			with open('./csvFiles/OuterRaceFault_vload_1.mat.csv', 'r') as f:
-			    mycsv = csv.reader(f)
-			    for row in itertools.islice(mycsv, bwstart,bwend):
-			        entries.append(row)
+
+for x in range(100):
+	rand = random.randint(1,5)
+	if rand == 1:
+		with open('./csvFiles/baseline_1.mat.csv', 'r') as f:
+			mycsv = csv.reader(f)
+			for row in itertools.islice(mycsv, bwstart,bwend):
+				row.append(270) #load
+				row.append(25)  #rate
+				row.append(0)
+				entries.append(row[1:])
+				
+	if rand == 3:
+		with open('./csvFiles/InnerRaceFault_vload_1.mat.csv', 'r') as f:
+			mycsv = csv.reader(f)
+			for row in itertools.islice(mycsv, bwstart,bwend):
+				row.append(0)
+				entries.append(row[1:])
+	if rand == 4:
+		with open('./csvFiles/OuterRaceFault_1.mat.csv', 'r') as f:
+			mycsv = csv.reader(f)
+			for row in itertools.islice(mycsv, bwstart,bwend):
+				row.append(1)
+				entries.append(row[1:])
+	if rand == 2:
+		with open('./csvFiles/OuterRaceFault_vload_1.mat.csv', 'r') as f:
+			mycsv = csv.reader(f)
+			for row in itertools.islice(mycsv, bwstart,bwend):
+				row.append(1)
+				entries.append(row[1:])
+
+	bwstart = bwend
+	bwend = bwend + 500
 
 with open('BaseLine-Faulty-merged.csv', 'wb') as f:
 	writer = csv.writer(f)
