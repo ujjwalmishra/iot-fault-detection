48a2abb5edda7442c7e959b011c315596c0726fd
chandhya <chandhya.ts@gmail.com>
2019-03-27 22:42:03 -0500

    Update scripts.js
    Updated scripts based on api calls in app.js.

--- visualization/public/scripts.js
+++ visualization/public/scripts.js
@@ -125,6 +125,78 @@ const loadData1 = () => {
       const data = [firstTrace];
       const layout = {
         title: 'G data for Inner fault',
+         type: "date",
+        xaxis: {
+          autotick: true,
+          ticks: 'outside',
+          tick0: 1553357710369071872,
+          dtick: 0.000000000000000000000025,
+          tickcolor: '#000'
+        },
+        yaxis: {
+          autotick: false,
+          ticks: 'outside',
+          tick0: 0,
+          dtick: 0.25,
+          ticklen: 8,
+          tickwidth: 4,
+          tickcolor: '#000'
+        }
+      };
+      return Plotly.newPlot('graphs-container1', data, layout);
+    })
+    .catch( error => console.log(error) );
+}
+
+const loadData2 = () => {
+  fetch('/api/v1/main/2')
+    .then( response => {
+      if (response.status !== 200) {
+        console.log(response);
+      }
+      return response;
+    })
+    .then(response => response.json())
+    .then(parsedResponse => {
+      const unpackData = (arr, key) => {
+        return arr.map(obj => obj[key])
+      }
+      const firstTrace = {
+        type: 'scatter',
+        mode: 'lines',
+        name: 'M',
+        x: unpackData(parsedResponse, 'time'),
+        y: unpackData(parsedResponse, 'g_data'),
+        line: {color: '#17BECF'}
+      }
+      // const secondTrace = {
+      //   type: "scatter",
+      //   mode: "lines",
+      //   name: 'Mean System Usage',
+      //   x: unpackData(parsedResponse, 'time'),
+      //   y: unpackData(parsedResponse, 'mean_usage_system'),
+      //   line: {color: '#7F7F7F'}
+      // }
+      const data = [firstTrace];
+      const layout = {
+        title: 'G data for Outer fault',
+         type: "date",
+        xaxis: {
+          autotick: true,
+          ticks: 'outside',
+          tick0: 1553357710369071872,
+          dtick: 0.000000000000000000000025,
+          tickcolor: '#000'
+        },
+        yaxis: {
+          autotick: false,
+          ticks: 'outside',
+          tick0: 0,
+          dtick: 0.25,
+          ticklen: 8,
+          tickwidth: 4,
+          tickcolor: '#000'
+        }
       };
       return Plotly.newPlot('graphs-container1', data, layout);
     })
